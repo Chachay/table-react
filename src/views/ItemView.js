276c16b2dc5c02c44eb8ffc97e141d5bc34f9631
Chachay <git@chachay.org>
2022-07-31 08:02:34 +0900

    Update style

--- src/views/ItemView.js
+++ src/views/ItemView.js
@@ -15,39 +15,45 @@ function ItemView(prop) {
   if(!post) return 'nopost';
 
   return (
-    <div className="row">
-      <div
-        className="col-11"
-      >
-        <h1 className="h2">{post.title}</h1>
-      </div>
-      <div
-        className="col-1"
-      >
-        <Link to={`/edit/${entryId}`}>
-          Edit
-        </Link>
-      </div>
-      <div
-        className="col-4"
-      >
-        {post.id}
-      </div>
-      <div
-        className="col-8"
-      >
-        {post.date}
-      </div>
-      <div
-        className="col-12"
-      >
-        <ReactMarkdown
-          components={{
-            h1: 'h2', h2: 'h3', h3: 'h4', h4: 'h5'
-          }}
+    <div className="p-4">
+      <div className="row">
+        <div
+          className="col-8"
         >
-          {post.body}
-        </ReactMarkdown>
+          {post.author}
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className="col-8 gray"
+        >
+          {post.date}
+        </div>
+        <div
+          className="col-1"
+        >
+          <Link to={`/edit/${entryId}`}>
+            Edit
+          </Link>
+        </div>
+      </div>
+      <div className="row">
+        <div
+          className="col-12"
+        >
+          <h1 className="h2">{post.title}</h1>
+        </div>
+        <div
+          className="col-12"
+        >
+          <ReactMarkdown
+            components={{
+              h1: 'h2', h2: 'h3', h3: 'h4', h4: 'h5'
+            }}
+          >
+            {post.body}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   );
